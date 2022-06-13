6e5a157508d08cf9aae66f594579da20eb90dd14
anitaloggg <huichen.lo@gmail.com>
2022-06-13 16:43:24 +0800

    main

---
+++ projects/mod04/src/app/page4/page4.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page4',
+  templateUrl: './page4.component.html',
+  styles: [
+  ]
+})
+export class Page4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  SayHi() {
+    alert("Hello");
+  }
+  onClick(element: string) {
+    console.log(element);
+  }
+  onClick2(event: Event, element: string) {
+    console.log(element);
+    console.log(event.target);
+    console.log(event.currentTarget);
+  }
+  onClick3(event: Event, element: string) {
+    console.log(element);
+    console.log(event.target);
+    console.log(event.currentTarget);
+    event.preventDefault();
+    // event.stopPropagation();    
+    event.stopImmediatePropagation();
+  }
+
+  result: string = "";
+  onchange(event: Event) {
+    let selectElement: HTMLSelectElement = <HTMLSelectElement>event.target;
+    console.log(selectElement.value);
+    this.result = selectElement.value;
+  }
+
+}
