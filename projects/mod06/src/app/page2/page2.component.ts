4dc92848dc819cf2abf113d4b65d0392fd908ca9
anitaloggg <huichen.lo@gmail.com>
2022-06-15 09:23:29 +0800

    main

---
+++ projects/mod06/src/app/page2/page2.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MeetingRoom } from '../meeting-room';
+
+@Component({
+  selector: 'app-page2',
+  templateUrl: './page2.component.html',
+  styles: [
+  ]
+})
+export class Page2Component implements OnInit {
+  public meetingRoom: MeetingRoom;
+  public capacity = ['5人', '10人', '20人', '30人', '40人']
+
+  constructor() {
+    this.meetingRoom = new MeetingRoom(
+      101, '哥多華', this.capacity[2], false, true);
+  }
+
+
+
+  ngOnInit(): void {
+  }
+  submitted: Boolean = false;
+  onSubmit(roomForm: NgForm) {
+    if (roomForm.valid)
+      console.log(roomForm.value);
+    this.submitted = true;
+  }
+
+}
