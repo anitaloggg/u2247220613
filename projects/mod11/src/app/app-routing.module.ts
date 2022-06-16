228d7e931f60e23954e18306255900d3b528b2c3
anitaloggg <huichen.lo@gmail.com>
2022-06-16 16:25:16 +0800

    main

--- projects/mod11/src/app/app-routing.module.ts
+++ projects/mod11/src/app/app-routing.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Add2CalendarComponent } from './booking/add2-calendar/add2-calendar.component';
 import { BookingDetailComponent } from './booking/booking-detail/booking-detail.component';
 import { BookingComponent } from './booking/booking.component';
+import { RentalDeviceComponent } from './booking/rental-device/rental-device.component';
 import { MeetingRoomsAppComponent } from './meeting-rooms-app/meeting-rooms-app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
- { path:'', redirectTo:'booking', pathMatch:'full'},
+  { path: '', redirectTo: 'booking', pathMatch: 'full' },
 
-  {path:'booking', component: BookingComponent}, 
-  {path:'booking/:id', component: BookingDetailComponent}, 
+  { path: 'booking', component: BookingComponent },
+  {
+    path: 'booking/:id', component: BookingDetailComponent,
+    children: [
+      { path: 'calendar', component: Add2CalendarComponent },
+      { path: 'device', component: RentalDeviceComponent }
+    ]
+  },
 
-  {path:'meetingroomapp', component: MeetingRoomsAppComponent}, 
+  { path: 'meetingroomapp', component: MeetingRoomsAppComponent },
 
   { path: '**', component: PageNotFoundComponent }
 
