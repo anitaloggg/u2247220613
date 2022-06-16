fe6b7e06151be209f1c1cb9117bbc7da8f712363
anitaloggg <huichen.lo@gmail.com>
2022-06-16 15:39:58 +0800

    main

--- projects/mod11/src/app/app-routing.module.ts
+++ projects/mod11/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookingComponent } from './booking/booking.component';
+import { MeetingRoomsAppComponent } from './meeting-rooms-app/meeting-rooms-app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path:'', redirectTo:'booking', pathMatch:'full'},
+
+  {path:'booking', component: BookingComponent}, 
+  {path:'meetingroomapp', component: MeetingRoomsAppComponent}, 
+
+  { path: '**', component: PageNotFoundComponent }
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
