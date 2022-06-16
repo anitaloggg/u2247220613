44feeda17608bad9083d3debde345b8661003889
anitaloggg <huichen.lo@gmail.com>
2022-06-16 10:49:04 +0800

    main

--- projects/mod10/src/app/app.module.ts
+++ projects/mod10/src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -12,7 +14,10 @@ import { MeetingRoomListComponent } from './meeting-room-list/meeting-room-list.
     MeetingRoomListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule,
+    HttpClientModule 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projects/mod10/src/app/shared/meeting-room.service.ts
+++ projects/mod10/src/app/shared/meeting-room.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MeetingRoom } from './meeting-room';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MeetingRoomService {
+  public formData = new MeetingRoom();
+  readonly rootUrl = 'https://localhost:44356/MeetingRooms'
+  public list: MeetingRoom[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getList() {
+    this.http.get<MeetingRoom[]>(this.rootUrl)
+      .subscribe(resp => this.list = resp);
+  }
 }
